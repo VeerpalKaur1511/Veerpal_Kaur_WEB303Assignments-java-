0f59cc8bfe77cfea701dcebf474b6d34ee3e2c08
VeerpalKaur1511 <122822806+VeerpalKaur1511@users.noreply.github.com>
2023-09-29 13:36:16 -0400

    i updated my code in script.js

--- WEB303As03/js/script.js
+++ WEB303As03/js/script.js
@@ -5,43 +5,47 @@
 // // Create a jquery using $.getJSON that uploads the team info
 
 function getJsonData (){
-    $.getJSON("team.json", function(data){
-    console.log(data);
-      $.each(data.members, function(index, obj){
-        $('div#team').append($("<h2>" + obj.name + "</h2>"));
-        $('div#team').append($("<h5>" + obj.position + "</h5>"));
-        $('div#team').append($("<p>" + obj.bio + "</p>"));
-      });
+  $.getJSON("team.json", function(data){
+  console.log(data);
+    $.each(data.members, function(index, obj){
+      $('div#team').append($("<h2>" + obj.name + "</h2>"));
+      $('div#team').append($("<h5>" + obj.position + "</h5>"));
+      $('div#team').append($("<p>" + obj.bio + "</p>"));
     });
-  };
+  });
+};
 
 $(document).ready(function(){
-   getDataByAjax();
-   //getJsonData();
+ getDataByAjax();
+ //getJsonData();
 });
 // using the same jquery using $.ajax
 
-    function getDataByAjax(){
-      // Display "Loading..." before sending the AJAX request
+  function getDataByAjax(){
+    // Display "Loading..." before sending the AJAX request
+    $('div#team').append($("<p>" + "Loading..." + "</p>"));
+    $.ajax({
+      type: "GET",
+      url: "team.json",
+      datatype: "json",
+      beforeSend: function(){
+      // This callback runs before the AJAX request is sent
       $('div#team').append($("<p>" + "Loading..." + "</p>"));
-      $.ajax({
-        type: "GET",
-        url: "team.json",
-        datatype: "json",
-        beforeSend: function(){
-
-        },
-        success: function(result){
-            console.log(result);
-            $.each(result.members, function(index, obj){
-              $('p:first-of-type').fadeOut(2000);
-              $('div#team').append($("<h2>" + obj.name + "</h2>"));
-              $('div#team').append($("<h5>" + obj.position + "</h5>"));
-              $('div#team').append($("<p>" + obj.bio + "</p>"));
-            });
-        },
-        error: function(error) {
-          console.error('Error:', error);
-        }
-      });
-    };
+      },
+      success: function(result){
+          console.log(result);
+          $.each(result.members, function(index, obj){
+            $('p:first-of-type').fadeOut(2000);
+            $('div#team').append($("<h2>" + obj.name + "</h2>"));
+            $('div#team').append($("<h5>" + obj.position + "</h5>"));
+            $('div#team').append($("<p>" + obj.bio + "</p>"));
+          });
+          setTimeout(function () {
+            $("#team").empty();
+          }, 3000);
+      },
+      error: function() {
+        $("#team").text("Error: Content could not be retrieved.");
+      }
+    });
+  };
